5afc958d9ed53716803e80940f09f7ad406ac630
Farhan <azzassfa@gmail.com>
2018-11-27 00:10:04 +0500

    Added mode functionality

--- meanmedianmode.py
+++ meanmedianmode.py
@@ -11,6 +11,32 @@ def median(lst):
     return median
 
 def mode(lst):
+
+    # initialize 2 arrays one for numbers and one for occurences
+    nums=[]
+    occurences = []
+    found =0
+    mode=0
+    modeCount=0
+
+    for i in range(len(lst)):                   # run a loop for items in lst
+        for j in range(len(occurences)):        # run another loop for checking occurence in occurences array
+            if lst[i] == nums[j]:
+                found=1                         # if found, increase count and set flag
+                nums[j] = lst[i]
+                occurences[j]+=1
+                if occurences[j] > modeCount:   # check which element is mode
+                    modeCount=occurences[j]
+                    mode=nums[j]
+        if found == 0:                            # when loops end if flag not fooung add entry
+            nums.append(lst[i])
+            occurences.append(1)
+            if 1 > modeCount:
+                modeCount=1
+                mode=lst[i]
+
+    return mode
+
     mode = "Mode note available"
     return mode
 
@@ -19,5 +45,5 @@ def meanmedianmode(lst):
     print(median(lst))
     print (mode(lst))
 
-a=[1,2,3,4,5,6,100]
+a=[2,4,6,8,10,2]
 meanmedianmode(a)
